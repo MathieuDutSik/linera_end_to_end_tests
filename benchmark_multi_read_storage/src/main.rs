671c60ea873b9ad03f4bfe209081c7e9f4857192
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-10-24 08:06:28 +0200

    Restructuration of the code. Now the test passes and yield the expected results.

--- benchmark_multi_read_storage/src/main.rs
+++ benchmark_multi_read_storage/src/main.rs
@@ -8,6 +8,7 @@ use linera_views::{
     rocks_db::RocksDbDatabase,
     scylla_db::ScyllaDbDatabase,
     store::{
+        KeyValueStore,
         ReadableKeyValueStore,
         TestKeyValueDatabase,
         WritableKeyValueStore,
@@ -31,7 +32,8 @@ fn get_key(rng: &mut impl Rng, len: usize) -> Vec<u8> {
 
 async fn test_storage_multi_read_kernel<S>(num_key: usize, key_size: usize, value_size: usize) -> Result<()>
 where
-    S: TestKeyValueDatabase + WritableKeyValueStore + ReadableKeyValueStore,
+    S: TestKeyValueDatabase,
+    S::Store: Clone + KeyValueStore,
 {
     let store = S::new_test_store().await?;
     let name = S::get_name();
@@ -54,25 +56,25 @@ where
     //
     let time = Instant::now();
     let values: Vec<Option<Vec<u8>>> = store.read_multi_values_bytes(keys1).await?;
-    println!("Runtime {name} for multi_read: {}ms", time.elapsed().as_millis() as f64);
+    println!("Runtime {name} for multi_read: {}ms", time.elapsed().as_micros() as f64);
     assert_eq!(values, read_values);
     //
     let time = Instant::now();
     let mut values: Vec<Option<Vec<u8>>> = Vec::new();
     for key in keys2 {
-        values.push(store.read_value_bytes(key).await?);
+        values.push(store.read_value_bytes(&key).await?);
     }
-    println!("Runtime {name} for loop read: {}ms", time.elapsed().as_millis() as f64);
+    println!("Runtime {name} for loop read: {}ms", time.elapsed().as_micros() as f64);
     assert_eq!(values, read_values);
     //
     let time = Instant::now();
     let mut futures = Vec::new();
     for key in keys3 {
         let store = store.clone();
-        futures.push(async move { store.read_value_bytes(key).await });
+        futures.push(async move { store.read_value_bytes(&key).await });
     }
     let values: Vec<Option<Vec<u8>>> = futures::future::try_join_all(futures).await?;
-    println!("Runtime {name} for futures read: {}ms", time.elapsed().as_millis() as f64);
+    println!("Runtime {name} for futures read: {}ms", time.elapsed().as_micros() as f64);
     assert_eq!(values, read_values);
     //
     Ok(())
@@ -93,9 +95,9 @@ async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 4 {
-        eprintln!("Error: No test specified");
-        eprintln!("Usage: {} <test-name>", args[0]);
-        eprintln!("Available tests: repeated-fungible, repeated-fungible-no-graphql, all");
+        eprintln!("Error: argument specified");
+        eprintln!("Usage: {} [num_key] [key_size] [value_size]", args[0]);
+        eprintln!("e.g. num_key = 100, key_size = 10, value_size = 10000");
         std::process::exit(1);
     }
 
@@ -103,9 +105,9 @@ async fn main() -> Result<()> {
     let key_size = &args[2];
     let value_size = &args[3];
 
-    let num_key = usize::try_from(num_key)?;
-    let key_size = usize::try_from(key_size)?;
-    let value_size = usize::try_from(value_size)?;
+    let num_key = num_key.parse::<usize>()?;
+    let key_size = key_size.parse::<usize>()?;
+    let value_size = value_size.parse::<usize>()?;
 
     test_various_storage(num_key, key_size, value_size).await?;
 
