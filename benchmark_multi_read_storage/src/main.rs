af5f821ab2646ac25700e5db6d05ee3c2cd366cf
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-10-24 17:10:07 +0200

    Add the writing benchmarks.

--- benchmark_multi_read_storage/src/main.rs
+++ benchmark_multi_read_storage/src/main.rs
@@ -28,6 +28,68 @@ fn get_key(rng: &mut impl Rng, len: usize) -> Vec<u8> {
     v
 }
 
+async fn test_storage_multi_write_kernel<S>(num_key: usize, key_size: usize, value_size: usize) -> Result<()>
+where
+    S: TestKeyValueDatabase,
+    S::Store: Clone + KeyValueStore,
+{
+    let name = S::get_name();
+    let mut rng = rand::rngs::StdRng::seed_from_u64(134 as u64);
+    let mut key_values1 = Vec::new();
+    let mut key_values2 = Vec::new();
+    let mut key_values3 = Vec::new();
+    for _ in 0..num_key {
+        let key = get_key(&mut rng, key_size);
+        let value = get_key(&mut rng, value_size);
+        key_values1.push((key.clone(), value.clone()));
+        key_values2.push((key.clone(), value.clone()));
+        key_values3.push((key, value));
+    }
+
+    //
+    // Mukti write
+    //
+    let store1 = S::new_test_store().await?;
+    let time = Instant::now();
+    let mut batch = Batch::new();
+    for (key, value) in key_values1 {
+        batch.put_key_value_bytes(key, value);
+    }
+    store1.write_batch(batch).await?;
+    println!("Runtime {name} for   multi write: {}ms", time.elapsed().as_micros() as f64);
+
+    //
+    // Direct write
+    //
+    let store2 = S::new_test_store().await?;
+    let time = Instant::now();
+    for (key, value) in key_values2 {
+        let mut batch = Batch::new();
+        batch.put_key_value_bytes(key, value);
+        store2.write_batch(batch).await?;
+    }
+    println!("Runtime {name} for    loop write: {}ms", time.elapsed().as_micros() as f64);
+
+    //
+    // Futures write
+    //
+    let store3 = S::new_test_store().await?;
+    let time = Instant::now();
+    let mut futures = Vec::new();
+    for (key, value) in key_values3 {
+        let store = store3.clone();
+        futures.push(async move {
+            let mut batch = Batch::new();
+            batch.put_key_value_bytes(key, value);
+            store.write_batch(batch).await
+        });
+    }
+    futures::future::try_join_all(futures).await?;
+    println!("Runtime {name} for futures write: {}ms", time.elapsed().as_micros() as f64);
+    Ok(())
+}
+
+
 
 
 async fn test_storage_multi_read_kernel<S>(num_key: usize, key_size: usize, value_size: usize) -> Result<()>
@@ -56,7 +118,7 @@ where
     //
     let time = Instant::now();
     let values: Vec<Option<Vec<u8>>> = store.read_multi_values_bytes(keys1).await?;
-    println!("Runtime {name} for multi_read: {}ms", time.elapsed().as_micros() as f64);
+    println!("Runtime {name} for   multi read: {}ms", time.elapsed().as_micros() as f64);
     assert_eq!(values, read_values);
     //
     let time = Instant::now();
@@ -64,7 +126,7 @@ where
     for key in keys2 {
         values.push(store.read_value_bytes(&key).await?);
     }
-    println!("Runtime {name} for loop read: {}ms", time.elapsed().as_micros() as f64);
+    println!("Runtime {name} for    loop read: {}ms", time.elapsed().as_micros() as f64);
     assert_eq!(values, read_values);
     //
     let time = Instant::now();
@@ -80,10 +142,22 @@ where
     Ok(())
 }
 
+async fn test_storage_multi_kernel<S>(num_key: usize, key_size: usize, value_size: usize) -> Result<()>
+where
+    S: TestKeyValueDatabase,
+    S::Store: Clone + KeyValueStore,
+{
+    println!("------------------------------------");
+    test_storage_multi_write_kernel::<S>(num_key, key_size, value_size).await?;
+    test_storage_multi_read_kernel::<S>(num_key, key_size, value_size).await?;
+    Ok(())
+}
+
+
 async fn test_various_storage(num_key: usize, key_size: usize, value_size: usize) -> Result<()> {
-    test_storage_multi_read_kernel::<DynamoDbDatabase>(num_key, key_size, value_size).await?;
-    test_storage_multi_read_kernel::<ScyllaDbDatabase>(num_key, key_size, value_size).await?;
-    test_storage_multi_read_kernel::<RocksDbDatabase>(num_key, key_size, value_size).await?;
+    test_storage_multi_kernel::<DynamoDbDatabase>(num_key, key_size, value_size).await?;
+    test_storage_multi_kernel::<ScyllaDbDatabase>(num_key, key_size, value_size).await?;
+    test_storage_multi_kernel::<RocksDbDatabase>(num_key, key_size, value_size).await?;
     Ok(())
 }
 
