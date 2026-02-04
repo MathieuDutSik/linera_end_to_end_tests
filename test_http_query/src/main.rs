09a486b4387a51e0494514f8b43c98e7ad31258b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2026-02-04 11:31:50 +0100

    Changes that allow to isolate the problem.

--- test_http_query/src/main.rs
+++ test_http_query/src/main.rs
@@ -5,7 +5,6 @@ use linera_service::cli_wrappers::{
     local_net::{get_node_port, Database, LocalNetConfig, ProcessInbox},
     LineraNet, LineraNetConfig, Network,
 };
-use std::path::PathBuf;
 
 const COINBASE_URL: &str = "https://api.exchange.coinbase.com/products/BTC-USD/trades";
 const KRAKEN_URL: &str = "https://api.kraken.com/0/public/Trades?pair=XBTUSDT";
@@ -14,10 +13,13 @@ fn get_config() -> LocalNetConfig {
     let mut config = LocalNetConfig::new_test(Database::Service, Network::Grpc);
     config.num_initial_validators = 1;
     config.num_shards = 1;
+//    config.http_request_allow_list = Some(vec![
+//        "localhost".to_owned(),
+//        "api.exchange.coinbase.com".to_owned(),
+//        "api.kraken.com".to_owned(),
+//    ]);
     config.http_request_allow_list = Some(vec![
-        "localhost".to_owned(),
         "api.exchange.coinbase.com".to_owned(),
-        "api.kraken.com".to_owned(),
     ]);
     config
 }
@@ -31,16 +33,15 @@ async fn main() -> Result<()> {
     let (mut net, client) = config.instantiate().await?;
     let chain = client.load_wallet()?.default_chain().unwrap();
 
-    let workspace_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let app_path = workspace_root.join("smart_contract_code");
-    let (contract, service) = client
-        .build_application(&app_path, "http-query", true)
-        .await?;
+    let name1 = "http-query";
+    let path1 = std::env::current_dir()?.join("./smart_contract_code/").join(name1);
+    let (contract_path, service_path) =
+        client.build_application(&path1, name1, true).await?;
 
     let application_id = client
         .publish_and_create::<HttpQueryAbi, (), ()>(
-            contract,
-            service,
+            contract_path,
+            service_path,
             VmRuntime::Wasm,
             &(),
             &(),
@@ -61,6 +62,7 @@ async fn main() -> Result<()> {
         .ok_or_else(|| anyhow::anyhow!("Unexpected Coinbase response: {coinbase_value}"))?;
     anyhow::ensure!(coinbase_len > 0, "Coinbase response length is zero");
 
+/*
     let kraken_value = application
         .run_json_query(HttpQueryRequest::HttpGet(KRAKEN_URL.to_owned()))
         .await?;
@@ -68,10 +70,11 @@ async fn main() -> Result<()> {
         .as_u64()
         .ok_or_else(|| anyhow::anyhow!("Unexpected Kraken response: {kraken_value}"))?;
     anyhow::ensure!(kraken_len > 0, "Kraken response length is zero");
+*/
 
     node_service.ensure_is_running()?;
     net.ensure_is_running().await?;
     net.terminate().await?;
-
+    println!("Normal termination of the program");
     Ok(())
 }
